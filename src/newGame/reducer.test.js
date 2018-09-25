9ee878292bdca30aa58c1dfd683b7ad7319c213b
Ramya <ramya@CHN-Ramyas-Macbook-Pro.local>
2018-09-25 13:27:13 +0530

    Story 1: Team Score updation

--- src/newGame/reducer.test.js
+++ src/newGame/reducer.test.js
@@ -5,4 +5,14 @@ describe('gameInformation/reducer', () => {
   it('should return initial state of 11 players in each team', () => {
     expect(reducer(undefined, {})).toEqual(initialState);
   });
+
+  it('should return given team1 name as India', () => {
+    const modifiedState = reducer(undefined, {});
+    expect(modifiedState.team1.name).toEqual('India');
+  });
+
+  it('should return given team2 name as Afghanistan', () => {
+    const modifiedState = reducer(undefined, {});
+    expect(modifiedState.team2.name).toEqual('Afghanistan');
+  });
 });
--- src/scorer/Scoreboard.jsx
+++ src/scorer/Scoreboard.jsx
@@ -1,36 +1,66 @@
 import React from 'react';
-import {Container,Row,Col} from 'reactstrap';
+import { Container, Row, Col } from 'reactstrap';
+import { connect } from 'react-redux';
 
+const totalOversPassed = (totalBalls) => {
+  const totalovers = `${parseInt((totalBalls / 6), 10)}.${(totalBalls % 6)}`;
+  return totalovers;
+};
 
-const ScoreBoard = () =>
-  <Container>
-    <br/>
+const isBowlingTeamAlreadyPlayed = (bowlingTeam) => {
+  if (bowlingTeam.totalBalls > 0) {
+    return `${bowlingTeam.name}  scored ${bowlingTeam.totalRun}/${bowlingTeam.totalWickets}   in  ${totalOversPassed(bowlingTeam.totalBalls)} overs `;
+  }
+  return '';
+};
+
+const ScoreBoard = props =>
+  (<Container>
+    <br />
     <Row>
-      <Col md= {{size:6,offset:3}}>
+      <Col md={{ size: 6, offset: 3 }}>
         <Row>
           <Col md="5" xs="4">
-            <b>Team 1</b>
+            <b>{props.battingTeam.name}</b>
           </Col>
           <Col sm="1" xs="2" />
-          <Col style ={{textAlign:"right"}}>
-            <b>120/5 in 12.1/20</b>
+          <Col style={{ textAlign: 'right' }}>
+            <b>{`${props.battingTeam.totalRun }/${props.battingTeam.totalWickets }  in  ${totalOversPassed(props.battingTeam.totalBalls)}/${props.maxOvers}`}</b>
           </Col>
         </Row>
       </Col>
     </Row>
-    <br/>
+    <br />
     <Row>
-      <Col md= {{size:6,offset:3}} sm="12">
+      <Col md={{ size: 6, offset: 3 }} sm="12">
         <Row>
           <Col>
-            Team 2 scored
-          </Col>
-          <Col style ={{textAlign:"right"}}>
-            <b>120/5 in 12.1/20</b>
+            {isBowlingTeamAlreadyPlayed(props.bowlingTeam)}
           </Col>
         </Row>
       </Col>
     </Row>
-  </Container>;
+   </Container>
+  );
 
-export default ScoreBoard;
+
+const mapStateToProps = (state) => {
+  let battingTeam;
+  let bowlingTeam;
+  if (state.gameInformation.team1.isBatting === true) {
+    battingTeam = state.gameInformation.team1;
+    bowlingTeam = state.gameInformation.team2;
+  } else {
+    battingTeam = state.gameInformation.team2;
+    bowlingTeam = state.gameInformation.team1;
+  }
+  return {
+    battingTeam,
+    bowlingTeam,
+    maxOvers: state.gameInformation.maxOvers,
+  };
+};
+
+const connectedTeamInfo = connect(mapStateToProps)(ScoreBoard);
+export default connectedTeamInfo;
+
