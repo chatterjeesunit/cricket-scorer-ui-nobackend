1570f83447a39adce156fea1942d35325645d35e
Sunit Chatterjee <34455433+chatterjeesunit@users.noreply.github.com>
2018-09-25 16:12:52 +0530

    new batsman

--- src/newBatsman/NewBatsman.jsx
+++ src/newBatsman/NewBatsman.jsx
@@ -1,22 +1,83 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import { connect } from 'react-redux';
+import { Container, Row, Col } from 'reactstrap';
 import React from 'react';
 import { selectNewBatsmanAction } from '../home/actions';
-import {connect} from 'react-redux';
+import PlayerStatus from '../newGame/gameConstants';
 
-const NewBatsman = (props) => (
+
+const NewBatsman = props => (
   <div>
-    Select New Batsman
+    <Container>
+      <Row>
+        <Col></Col>
+        <Col>
+          <h3>Select next batsman for {props.teamName}</h3>
+        </Col>
+        <Col></Col>
+      </Row>
+      <form>
+        <br />
+        {
+          props.players.map(player => (
+          <Row>
+            <Col/>
+
+            <Col>
+              <input
+                type="radio"
+                name="1"
+                value={player.id}
+                onClick={(event) => {
+                  this.selectedBatsman = event.target.value;
+                }}
+              />
+              <label>&nbsp;&nbsp;&nbsp;{player.name}</label>
+            </Col>
+
+            <Col/>
+          </Row>
+          ),)
+        }
+        <br />
+        <Row>
+          <Col></Col>
+          <Col>
+            <button
+              className="btn btn-outline-info button"
+              id="submit"
+              type="button"
+              onClick={props.onSelectBatsman}
+            >
+              Submit
+            </button>
+          </Col>
+          <Col></Col>
+        </Row>
+        
+      </form>
+
+    </Container>
   </div>
+
 );
 
 const mapStateToProps = (state) => {
-
+  const battingTeam = state.gameInformation.team1.isBatting ? state.gameInformation.team1 : state.gameInformation.team2;
+  return {
+    teamName: battingTeam.name,
+    players: [
+      ...battingTeam.players.filter(player => player.status === PlayerStatus.YET_TO_PLAY),
+    ],
+  };
 };
 
-const mapDispatchToProps = (dispatch) => {
-    onBatsmanSelect: (batsmanId) => {
-        selectNewBatsmanAction.batsmanId = batsmanId;
-        dispatch(onBatsmanSelect);
-    }
-};
+const mapDispatchToProps = (dispatch) => ({
+    onSelectBatsman: () => {
+      console.log(this.selectedBatsman);
+      selectNewBatsmanAction.batsmanId = this.selectedBatsman;
+      dispatch(selectNewBatsmanAction);
+    },
+  });
 
 export default connect(mapStateToProps, mapDispatchToProps)(NewBatsman);
