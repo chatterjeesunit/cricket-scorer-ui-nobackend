db91adc93216525b194794a421e9989e62570a83
sushant.sagar <sushant.sagar@INSushantss-MacBook-Pro.local>
2018-09-26 14:48:46 +0530

    fixed breaking build

--- src/scorer/Scorer.jsx
+++ src/scorer/Scorer.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ScoreBoard from './Scoreboard';
 import RunRecorder from './RunRecorder';
-import '../css/style.css';
 
 const Scorer = () => (
   <div className="center">
