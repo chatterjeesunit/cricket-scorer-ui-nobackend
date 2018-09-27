41cc8e3a724f14c48b21b0f3eada10bce7ff36f9
Chetana Dixit <chetana.dixit@Chetanas-MacBook-Pro.local>
2018-09-27 15:09:33 +0530

    Score board changes added

--- src/gameDetails/GameDetails.jsx
+++ src/gameDetails/GameDetails.jsx
@@ -1,26 +1,19 @@
 import React from 'react';
-import { Button } from 'reactstrap';
+import { Button, Container, Row, Col } from 'reactstrap';
 import { Routes } from '../routes/routes';
-import { Container, Row, Col } from 'reactstrap';
-import ScoreBoard  from '../scorer/Scoreboard'
+import ScoreBoard from '../scorer/Scoreboard'
 
 const GameDetails = (props) => (
   <div className="center">
-   <Container>
-    <div className="container">
-      <div className="row">
-        <div className="col-md-8 offset-2">
-          <div>
-            <Button className="button" size="lg" color="info" href={"#" + Routes.SCORER}>Back</Button>
-          </div>
-          
-          </div>
-        </div>
-      </div>
-      <ScoreBoard />
-      </Container>
-    </div>
+    <Container>
+      <Row>
+        <Col md={{ size: 8, offset: 2 }} sm={{ size: 12 }} className="scoreBoard">
+          <Button className="button" size="lg" color="info" href={"#" + Routes.SCORER}>Back</Button>
+        </Col>
+      </Row>
+      <ScoreBoard pageName="gameDetails" />
+    </Container>
+  </div>
+);
 
-    );
-    
 export default GameDetails;
