b16dee0dc978c7ca77c70b701aa0399efa72aa69
Chetana Dixit <chetana.dixit@Chetanas-MacBook-Pro.local>
2018-09-27 13:03:10 +0530

    added Score Board component

--- src/gameDetails/GameDetails.jsx
+++ src/gameDetails/GameDetails.jsx
@@ -1,19 +1,26 @@
 import React from 'react';
 import { Button } from 'reactstrap';
 import { Routes } from '../routes/routes';
-const GameDetails = () => (
+import { Container, Row, Col } from 'reactstrap';
+import ScoreBoard  from '../scorer/Scoreboard'
+
+const GameDetails = (props) => (
   <div className="center">
+   <Container>
     <div className="container">
       <div className="row">
         <div className="col-md-8 offset-2">
           <div>
             <Button className="button" size="lg" color="info" href={"#" + Routes.SCORER}>Back</Button>
           </div>
+          
+          </div>
         </div>
       </div>
+      <ScoreBoard />
+      </Container>
     </div>
-  </div>
 
-);
-
+    );
+    
 export default GameDetails;
