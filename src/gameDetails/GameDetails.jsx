dab6a3d2a282b86817ea2718d9d5d1c4f5abd933
Chetana Dixit <chetana.dixit@Chetanas-MacBook-Pro.local>
2018-09-27 12:35:50 +0530

    : css correction

--- src/gameDetails/GameDetails.jsx
+++ src/gameDetails/GameDetails.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import { Button } from 'reactstrap';
-import { Routes } from '../routes/routes' ;
+import { Routes } from '../routes/routes';
 const GameDetails = () => (
-  <div className="container">
-  <div>
-    <Button className = "button" size="lg" color="info" href={"#"+ Routes.SCORER}>Run Recorder</Button>
+  <div className="center">
+    <div className="container">
+      <div className="row">
+        <div className="col-md-8 offset-2">
+          <div>
+            <Button className="button" size="lg" color="info" href={"#" + Routes.SCORER}>Back</Button>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
-  </div>
+
 );
 
 export default GameDetails;
