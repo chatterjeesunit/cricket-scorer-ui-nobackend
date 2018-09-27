a1e09e9f3f3a30867561ab1b79f872a6256f8722
Chetana Dixit <chetana.dixit@Chetanas-MacBook-Pro.local>
2018-09-27 12:29:16 +0530

    hyperlink created for game summary

--- src/gameDetails/GameDetails.jsx
+++ src/gameDetails/GameDetails.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
-
+import { Button } from 'reactstrap';
+import { Routes } from '../routes/routes' ;
 const GameDetails = () => (
+  <div className="container">
   <div>
-    This is the game details page
+    <Button className = "button" size="lg" color="info" href={"#"+ Routes.SCORER}>Run Recorder</Button>
+  </div>
   </div>
 );
 
